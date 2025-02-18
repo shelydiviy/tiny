a3f0024b969f5629711affb50c942854b6561e95
Shkrest OS <telephon.inet@gmail.com>
2025-02-19 01:11:35 +0300

    add asio hl2sdk

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-HL2SDK_DIR=""
-ASIO_DIR=""
+HL2SDK_DIR="/home/sprut/fake/hl2sdk"
+ASIO_DIR="/home/sprut/fake/asio/asio"
 
 cmake -DCMAKE_BUILD_TYPE=Release -DHL2SDK-CSGO:PATH=$HL2SDK_DIR -DASIO-SRC:PATH=$ASIO_DIR -DEXECUTABLE_OUTPUT_PATH=$PWD/build/Release
 make
